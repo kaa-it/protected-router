8da4cbc2e06dcecb058a83582f5df80704ce604f
Andrey Kruglov <kruglov.it.rs@gmail.com>
2023-03-27 04:42:37 +0300

    home page

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <div className="home">
+            <h1 className="title">Главная</h1>
+            <button className="button">Личный кабинет</button>
+        </div>
+    )
+}
