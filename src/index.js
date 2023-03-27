3d653f83c9f36d2333a8ad896151d353743f1692
Andrey Kruglov <kruglov.it.rs@gmail.com>
2023-03-27 11:50:28 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from "react-redux";
+import {store} from "./services/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Router>
-          <App />
+          <Provider store={store}>
+              <App />
+          </Provider>
       </Router>
   </React.StrictMode>
 );
