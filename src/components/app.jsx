b940999443ff14e68e6d07bfe3d5c40effbbac33
Andrey Kruglov <kruglov.it.rs@gmail.com>
2023-03-27 13:26:03 +0300

    new version

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,34 +5,25 @@ import {Profile} from "../pages/profile";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {checkUserAuth} from "../services/actions";
-import {ProtectedRoute} from "./protected-route";
+import {OnlyAuth, OnlyUnAuth} from "./protected-route";
 
 
 function App() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-      dispatch(checkUserAuth());
-  }, []);
+    useEffect(() => {
+        dispatch(checkUserAuth());
+    }, []);
 
-  return (
-      <div className="app">
-          <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/login" element={
-                  <ProtectedRoute onlyUnAuth={true}>
-                      <Login/>
-                  </ProtectedRoute>
-              } />
-              <Route path="/profile" element={
-                  <ProtectedRoute>
-                      <Profile />
-                  </ProtectedRoute>
-
-              } />
-          </Routes>
-      </div>
-  );
+    return (
+        <div className="app">
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<OnlyUnAuth component={Login} />} />
+                <Route path="/profile" element={<OnlyAuth component={Profile} />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,7 +1,11 @@
 import {Navigate, useLocation} from "react-router-dom";
 import {useSelector} from "react-redux";
+import React from "react";
 
-export const ProtectedRoute = ({onlyUnAuth = false, children}) => {
+export const OnlyAuth = ({component, ...rest}) => <Protected component={component} {...rest} />;
+export const OnlyUnAuth = ({component, ...rest}) => <Protected onlyUnAuth={true} component={component} {...rest} />;
+
+export const Protected = ({onlyUnAuth = false, component, ...rest}) => {
     const isAuthChecked = useSelector(store => store.user.isAuthChecked);
     const user = useSelector(store => store.user.user);
     const location = useLocation();
@@ -14,17 +18,17 @@ export const ProtectedRoute = ({onlyUnAuth = false, children}) => {
     if (onlyUnAuth && user) {
         // Пользователь авторизован, но запрос предназначен только для неавторизованных пользователей
         // Нужно сделать редирект на главную страницу или на тот адрес, что записан в location.state.from
-        const { from } = location.state || { from: { pathname: "/" } }
-        return <Navigate to={from} />;
+        const {from} = location.state || {from: {pathname: "/"}}
+        return <Navigate to={from}/>;
     }
 
 
     if (!onlyUnAuth && !user) {
         // Сервер не ответил
-        return <Navigate to="/login" state={{ from: location }} />;
+        return <Navigate to="/login" state={{from: location}}/>;
     }
 
     // !onlyUnAuth && user
 
-    return children;
+    return component({...rest});
 };
